a4858b62a08f9913abbf8e65d2922873d081b7af
Stanley Quek <xXBlackCarnagexx@gmail.com>
2017-03-25 20:42:41 +0800

    Added multiple new features

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-  for (i = 0; i < 16; i++)
-  {
+
+
+    for (i = 0; i < 16; i++){
       var row = $('<div class="row"></div>')
 
       for (j = 0; j < 16; j++) {
@@ -10,13 +11,50 @@ $(document).ready(function(){
       $("#container").append(row);
   }
 
+  $("#changepx").click(function(){
+    var gridSize = prompt("What dimensions should the new grid be? (enter 1-50 only)",16)
+    if (gridSize > 50) {
+      alert("Too big")
+    }
+    else{
+    $(".row").remove()
+
+    //remaking the grid
+      for (i = 0; i < gridSize; i++){
+          var row = $('<div class="row"></div>')
+
+          for (j = 0; j < gridSize; j++) {
+              $('<div class="unit"></div>').appendTo(row);
+          }
+
+          $("#container").append(row);
+      }
+    }
+
+  $("#container").css("height",""+ (gridSize*20+500) +"px")
+
+    //Drawing effect
+    $(".unit").mouseenter(function(){
+      $(this).addClass("color")
+    });
+    //reset
+
+    $("#reset").click(function(){
+      $(".unit").removeClass("color")
+      $(".unit").css("background-color","white")
+    });
+
+
+  });
+
+
   //Drawing effect
   $(".unit").mouseenter(function(){
-    $(this).addClass("gray")
+    $(this).addClass("color")
   });
 //reset
   $("#reset").click(function(){
-    $(".unit").removeClass("gray")
+    $(".unit").removeClass("color")
   });
 //hover on button
   $("button").hover(function(){
@@ -27,5 +65,35 @@ $(document).ready(function(){
     $(this).fadeTo('fast',1)
   });
 
-  
+//color randomizer
+  function color(){
+    var letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var u =0; u<6; u++){
+      color += letters[Math.floor(Math.random()*16)]
+    }
+    return color
+  }
+
+  $("#rainbow").click(function(){
+  $(".unit").off('mouseenter')
+  $(".unit").mouseenter(function(){
+    $(this).css("background-color", ""+color()+"")
+  });
+});
+
+  $("#default").click(function(){
+    $(".unit").off('mouseenter')
+    $(".unit").mouseenter(function(){
+      $(this).css("background-color","gray")
+    })
+  })
+
+  $("#user").click(function(){
+    $(".unit").off('mouseenter')
+    var value = prompt("Choose your color value(hexadecimal only)","#FF0000")
+    $(".unit").mouseenter(function(){
+      $(this).css("background-color",""+value+"")
+    })
+  })
 });
